749a29d3c5ec1c8bf25cea2bd973dca35d68ac02
ZhangYang <842810148@qq.com>
2021-06-30 18:55:40 +0800

    更换主题颜色

--- lib/moudules/theme.dart
+++ lib/moudules/theme.dart
@@ -9,23 +9,36 @@ class ThemeSetPage extends StatefulWidget {
 
 class _ThemeSetPageState extends State<ThemeSetPage> {
   final _logic = Get.find<ThemeLogic>();
-  Map<String, Color> themeColorMap = {
-    'gray': Colors.grey,
-    'blue': Colors.blue,
-    'blueAccent': Colors.blueAccent,
-    'cyan': Colors.cyan,
-    'deepPurple': Colors.purple,
-    'deepPurpleAccent': Colors.deepPurpleAccent,
-    'deepOrange': Colors.orange,
-    'green': Colors.green,
-    'indigo': Colors.indigo,
-    'indigoAccent': Colors.indigoAccent,
-    'orange': Colors.orange,
-    'purple': Colors.purple,
-    'pink': Colors.pink,
-    'red': Colors.red,
-    'teal': Colors.teal,
-    'black': Colors.black,
+  // Map<String, Color> themeColorMap = {
+  //   'gray': Colors.grey,
+  //   'blue': Colors.blue,
+  //   'blueAccent': Colors.blueAccent,
+  //   'cyan': Colors.cyan,
+  //   'deepPurple': Colors.purple,
+  //   'deepPurpleAccent': Colors.deepPurpleAccent,
+  //   'deepOrange': Colors.orange,
+  //   'green': Colors.green,
+  //   'indigo': Colors.indigo,
+  //   'indigoAccent': Colors.indigoAccent,
+  //   'orange': Colors.orange,
+  //   'purple': Colors.purple,
+  //   'pink': Colors.pink,
+  //   'red': Colors.red,
+  //   'teal': Colors.teal,
+  //   'black': Colors.black,
+  // };
+
+    Map<String, Color> themeColorMap = {
+    '大地棕': Color.fromRGBO(200, 142, 94, 1),
+    '经典蓝':  Color.fromRGBO(0, 163, 237, 1),
+    '活力红':  Color.fromRGBO(219, 25, 21, 1),
+    '酷炫黑':  Color.fromRGBO(40, 40, 40, 1),
+    '新鲜橙':  Color.fromRGBO(255, 90, 30, 1),
+    '健康绿':  Color.fromRGBO(59, 174, 94, 1),
+    '高贵紫':  Color.fromRGBO(84, 29, 121, 1),
+    '可爱粉':  Color.fromRGBO(255, 103, 142, 1),
+    '自然青':  Color.fromRGBO(0, 175, 137, 1),
+    '魅力黄':  Color.fromRGBO(255, 192, 83, 1),
   };
   String? _colorKey;
 
